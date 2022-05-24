3d8ae1c1514133a41e83884bd5f54fc93e966f99
Husseine KEITA <husseine.keita@gmail.com>
2022-05-24 19:00:43 +0200

    Gcp bucket acess and display

--- cage_viewer.py
+++ cage_viewer.py
@@ -2,57 +2,62 @@ import streamlit as st
 import cv2
 from google.oauth2 import service_account
 from google.cloud import storage
+from IPython.display import Image
+import pandas as pd
+import os
+from PIL import Image
+import io
+import numpy as np
 
-#credentials = service_account.Credentials.from_service_account_info(
-#    st.secrets["gcp_service_account"]
-#)
-credentials = service_account.Credentials.from_service_account_file(
-    'minos-350712-2dc2472d64bd.json')
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]=".streamlit/secrets.toml"
+
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"])
 
 client = storage.Client(credentials=credentials)
 bucket_name = 'cells_and_cages'
-print('#############################')
-print('#############################')
-print('#############################')
-print('#############################')
-print('#############################')
-print('#############################')
+bucket = client.bucket(bucket_name)
 
-def read_file(bucket_name, file_path):
-    bucket = client.bucket(bucket_name)
-    content = bucket.blob(file_path).download_as_string().decode("utf-8")
-    return content
+def get_image(bucket, file_path):
+    blb = bucket.blob(file_path)
+    bytes = blb.download_as_bytes()
+    image = Image.open(io.BytesIO(bytes))
+    return np.array(image)/65025
+
+
 
 
 header = st.container()
 dataset_explanation = st.container()
 viewer= st.container()
 
+
 image, image_rgb = st.columns((2,2))
 row_number, col_number = st.columns((1,1))
-
-example_image = cv2.imread('DATASET_CHIP71_1_7.tif')
-example_image_rgb = cv2.imread('DATASET_CHIP71composite_1_7.tif')
-example_image_rgb = cv2.cvtColor(example_image_rgb, cv2.COLOR_BGR2RGB)
+r, viewer_image, rr = st.columns(3)
+#example_image = cv2.imread('DATASET_CHIP71_1_7.tif')
+#example_image_rgb = cv2.imread('DATASET_CHIP71composite_1_7.tif')
+#example_image_rgb = cv2.cvtColor(example_image_rgb, cv2.COLOR_BGR2RGB)
 
 
 with header:
     st.title("Welcome to the Minos Biosciences cage viewer application!")
     st.text('This app displays cages from any chosen chip')
-    st.text('\n \n \n \n \n \n')
 
-with dataset_explanation:
-    st.title("Chips")
-    st.text('These images...')
+#with dataset_explanation:
+#    st.title("Chips")
+#    st.text('These images...')
 
 with viewer:
     st.title("Cage viewer")
-    st.text("Here you can choose a chip then a cage to see what it looks like.")
-    with image:
-        st.image(example_image)
+    #st.text("Here you can choose a chip then a cage to see what it looks like.")
+    
+    #with image:
+    #    st.image(example_image)
 
-    with image_rgb:
-        st.image(example_image_rgb)
+    #with image_rgb:
+    #    st.image(example_image_rgb)
     
     chip_number_choice= st.selectbox(
      'Select the chip you want to display',
@@ -83,10 +88,15 @@ with viewer:
             row_number_choice = st.selectbox(
                 'Select the row number of the cage you want to display',
                 tuple(str(i) for i in range(24)))
-
-filename = 'DATASET_CHIP_' + chip_number_choice + '_' + row_number_choice + '_' + col_number_choice + '.tif'
 st.text('Cage displayed: Chip %s \t Row number: %s \t Column number: %s'%(chip_number_choice, row_number_choice, col_number_choice) )
     
-#content = read_file(bucket_name=bucket_name, file_path="DATASET_CHIP_71/DATASET_CHIP71_1_7.tif")
-#img_content = cv2.imread(content)
-#st.image(img_content)
+
+filename= "DATASET_CHIP_" +chip_number_choice+ "/DATASET_CHIP" + chip_number_choice + "_" + row_number_choice + "_" + col_number_choice + ".tif"
+#print("L'image à afficher est %s"%(filename))
+
+with r:
+    st.write(' ')
+with viewer_image:
+    st.image(get_image(bucket=bucket,file_path=filename))
+with rr:
+    st.write(' ') 
