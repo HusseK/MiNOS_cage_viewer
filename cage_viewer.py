113b053b6b49816e56582f6a6cb8ac07ff68525e
Husseine KEITA <husseine.keita@gmail.com>
2022-06-15 14:28:45 +0200

    Add CHIP 72 & 74

--- cage_viewer.py
+++ cage_viewer.py
@@ -34,10 +34,12 @@ def get_image(bucket, file_path,ang=0, div=65535):
     blb = bucket.blob(file_path)
     bytes = blb.download_as_bytes()
     image = Image.open(io.BytesIO(bytes))
+    #st.image(rotateImage(np.array(image)/div, ang))
     return rotateImage(np.array(image)/div, ang)
 
 from matplotlib.patches import Rectangle
 
+#@st.cache()
 def show_cage_pos_on_chip(img, df, cage_id,chip_number, linewidth=1.5, save=False):
   fig, ax = plt.subplots()
   fig.set_figheight(15)
@@ -51,9 +53,10 @@ def show_cage_pos_on_chip(img, df, cage_id,chip_number, linewidth=1.5, save=Fals
   plt.gca().add_patch(Rectangle((ymin,xmin),288,288,linewidth=linewidth,edgecolor='r',facecolor='#3EABBD', alpha=0.2))
   plt.text(ymin+70, xmin-50, cage_id, color='white', fontsize=7)
   plt.axis('off')
-  if save:
-    plt.savefig("chip%s_box_around_cage_%s.png"%(chip_number, cage_id), bbox_inches='tight', pad_inches = 0, dpi=fig.dpi)
-  return "chip%s_box_around_cage_%s.png"%(chip_number, cage_id)
+  st.pyplot(fig)
+  #if save:
+  #  plt.savefig("chip%s_box_around_cage_%s.png"%(chip_number, cage_id), bbox_inches='tight', pad_inches = 0, dpi=fig.dpi)
+  #return "chip%s_box_around_cage_%s.png"%(chip_number, cage_id)
 
 
 
@@ -140,13 +143,13 @@ def main():
         
         chip_number_choice= st.selectbox(
         'Select the chip you want to display',
-        ('None','58', '66', '67', '69', '70', '71'))
+        ('None','58', '66', '67', '69', '70', '71','72', '74'))
         #st.write('You selected:', chip_number_choice)
         if chip_number_choice!='None':
-            #try:
-            raw_chip_image = get_image(bucket= bucket, file_path='RAW_CHIPS/CHIP%s.ome.jpg'%chip_number_choice, div=255, ang=float(angles[chip_number_choice]))
-            #except:
-            #    print("Problème de chargement de l'image")
+            try:
+                raw_chip_image = get_image(bucket= bucket, file_path='RAW_CHIPS/CHIP%s.ome.jpg'%chip_number_choice, div=255, ang=float(angles[chip_number_choice]))
+            except:
+                print("Problème de chargement de l'image")
 
             try:
                 df_cages_position = gcp_csv_to_df(bucket=bucket, file_path="cage_positions/df_CHIP_%s_cages_position.xlsx"%(chip_number_choice))
@@ -199,9 +202,11 @@ def main():
                     st.write(' ')
 
             with composite_image:
-                
                 try:
-                    im_composite= get_image(bucket=bucket, file_path="DATASET_CHIP_" +chip_number_choice+ "/DATASET_CHIP" + chip_number_choice + "composite_" + row_number_choice + "_" + col_number_choice + ".tif", div=255)
+                    if chip_number_choice in ['72', '74']:
+                        im_composite = get_image(bucket=bucket, file_path="DATASET_CHIP_" + chip_number_choice + "/COMPOSITE_DATASET_CHIP" + chip_number_choice + '_' + row_number_choice + "_" + col_number_choice + ".tif", div=255 )
+                    else:
+                        im_composite = get_image(bucket=bucket, file_path="DATASET_CHIP_" +chip_number_choice+ "/DATASET_CHIP" + chip_number_choice + "composite_" + row_number_choice + "_" + col_number_choice + ".tif", div=255)
                     st.image(im_composite, caption="Composite row %s column %s"%(row_number_choice,col_number_choice), width=image_width)
                 except:
                     st.write(' ')
@@ -217,12 +222,13 @@ def main():
         'Additional content',
         ['Whole chip %s'%(chip_number_choice), 'Single cell indexes', 'Locate cage %s_%s on chip %s'%(row_number_choice,col_number_choice,chip_number_choice)]#,     ['Whole chip %s'%(chip_number_choice)]
         )
+        #raw_chip_image = get_image(bucket= bucket, file_path='RAW_CHIPS/CHIP%s.ome.jpg'%chip_number_choice, div=255, ang=float(angles[chip_number_choice]))
         #st.write(options)
 
         #see_whole_chip = st.checkbox('See Chip %s'%chip_number_choice, help='Check this box if you want to see the whole chip', value=False)
         if 'Whole chip %s'%(chip_number_choice) in options:    
             try:
-                #
+                raw_chip_image = get_image(bucket= bucket, file_path='RAW_CHIPS/CHIP%s.ome.jpg'%chip_number_choice, div=255, ang=float(angles[chip_number_choice]))
                 st.image(raw_chip_image, use_column_width=True)
             except:
                 st.write(' ')
@@ -232,11 +238,15 @@ def main():
             except:
                 st.write('Single cell cages indices not available')
         if 'Locate cage %s_%s on chip %s'%(row_number_choice,col_number_choice,chip_number_choice) in options:
-            rr = show_cage_pos_on_chip(raw_chip_image , df_cages_position, "%s_%s"%(row_number_choice,col_number_choice),int(chip_number_choice),save=True)
-            cage_localisation_image = cv2.imread(rr)
-            cage_localisation_image = cv2.cvtColor(cage_localisation_image, cv2.COLOR_BGR2RGB)
-            st.image(cage_localisation_image, use_column_width=True)
-            os.remove(rr)
+            #try:
+            #raw_chip_image = get_image(bucket= bucket, file_path='RAW_CHIPS/CHIP%s.ome.jpg'%chip_number_choice, div=255, ang=float(angles[chip_number_choice]))
+            show_cage_pos_on_chip(raw_chip_image , df_cages_position, "%s_%s"%(row_number_choice,col_number_choice),int(chip_number_choice),save=True)
+                #cage_localisation_image = cv2.imread(rr)
+                #cage_localisation_image = cv2.cvtColor(cage_localisation_image, cv2.COLOR_BGR2RGB)
+                #st.image(cage_localisation_image, use_column_width=True)
+                #os.remove(rr)
+            #except:
+                #st.write('Cage localization not available')
 
 
         #see_whole_chip = st.checkbox('See Chip %s'%chip_number_choice, help='Check this box if you want to see the whole chip', value=False)
